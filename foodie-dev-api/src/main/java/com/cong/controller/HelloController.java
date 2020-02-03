ef53992097f17c30909408a618bcd6c155d14673
dengcong <jnudengcong@gmail.com>
2020-02-03 16:53:26 +0800

    添加测试 session 的接口：添加 session id

--- foodie-dev-api/src/main/java/com/cong/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/cong/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @ApiIgnore
 @RestController
 public class HelloController {
@@ -13,4 +16,14 @@ public class HelloController {
         return "Hello World!";
     }
 
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+//        session.removeAttribute("userInfo");
+
+        return "OK";
+    }
 }
