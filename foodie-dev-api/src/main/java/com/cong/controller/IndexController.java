3b5090e596478b5e0e4c532e3cfee91014c73734
dengcong <jnudengcong@gmail.com>
2020-02-04 15:49:22 +0800

    【商品分类】需求分析

--- foodie-dev-api/src/main/java/com/cong/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/cong/controller/IndexController.java
@@ -28,4 +28,10 @@ public class IndexController {
         return CONGJSONResult.ok(list);
     }
 
+    /**
+     * 首页分类展示需求
+     * 1. 第一次刷新主页查询大分类，渲染展示到首页
+     * 2. 如果鼠标上移到大分类，则加载其子分类的内容，如果已经存在子分类，则不需要加载（懒加载）
+     */
+
 }
