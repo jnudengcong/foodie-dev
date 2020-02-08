94791c86da50bdea9e843617742e4661edb5f4bd
dengcong <jnudengcong@gmail.com>
2020-02-08 23:11:45 +0800

    【购物车】添加从购物车中删除商品的接口

--- foodie-dev-api/src/main/java/com/cong/controller/ShopcartController.java
+++ foodie-dev-api/src/main/java/com/cong/controller/ShopcartController.java
@@ -37,4 +37,21 @@ public class ShopcartController {
 
         return CONGJSONResult.ok();
     }
+
+    @ApiOperation(value = "从购物车中删除商品", notes = "从购物车中删除商品", httpMethod = "POST")
+    @PostMapping("/del")
+    public CONGJSONResult del(
+            @RequestParam String userId,
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
+            return CONGJSONResult.errorMsg("参数不能为空");
+        }
+
+        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
+
+        return CONGJSONResult.ok();
+    }
 }
