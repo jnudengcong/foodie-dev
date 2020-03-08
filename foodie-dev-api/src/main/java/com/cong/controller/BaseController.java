780c86226a639508e0c2b61272f204b2aab835d0
jnudengcong <jundengcong@gmail.com>
2020-03-08 23:35:50 +0800

    【微信支付】向支付中心发送当前订单，用于保存支付中心的订单数据

--- foodie-dev-api/src/main/java/com/cong/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/cong/controller/BaseController.java
@@ -11,6 +11,9 @@ public class BaseController {
 
     public static final Integer PAGE_SIZE = 10;
 
+    // 支付中心的调用地址
+    public static final String paymentUrl = "http://payment.t.mukewang.com/foodie-payment/payment/createMerchantOrder";
+
     // 微信支付 -> 支付中心 -> 电商平台
     //                    |->    回调通知的url
     public static final String payReturnUrl = "http://localhost:8088/orders/notifyMerchantOrderPaid";
--- foodie-dev-api/src/main/java/com/cong/controller/OrdersController.java
+++ foodie-dev-api/src/main/java/com/cong/controller/OrdersController.java
@@ -10,11 +10,9 @@ import com.cong.utils.CONGJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,6 +24,9 @@ public class OrdersController extends BaseController {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
     @PostMapping("/create")
@@ -43,14 +44,30 @@ public class OrdersController extends BaseController {
         // 1. 创建订单
         OrderVO orderVO = orderService.createOrder(submitOrderBO);
         String orderId = orderVO.getOrderId();
-        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
-        merchantOrdersVO.setReturnUrl(payReturnUrl);
 
         // 2. 创建订单以后，移除购物车中已结算（已提交）的商品
         // TODO 整合redis之后，完善购物车中的已结算商品清除，并且同步到前端的cookie
 //        CookieUtils.setCookie(request, response, FOODIE_SHOPCART, "", true);
 
         // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
+        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
+        merchantOrdersVO.setReturnUrl(payReturnUrl);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("imoocUserId", "imooc");
+        headers.add("password", "imooc");
+
+        HttpEntity<MerchantOrdersVO> entity = new HttpEntity<>(merchantOrdersVO, headers);
+
+        ResponseEntity<CONGJSONResult> responseEntity = restTemplate.postForEntity(paymentUrl,
+                                                                                    entity,
+                                                                                    CONGJSONResult.class);
+
+        CONGJSONResult paymentResult = responseEntity.getBody();
+        if (paymentResult.getStatus() != HttpStatus.OK.value()) {
+            return CONGJSONResult.errorMsg("支付中心订单创建失败，请联系管理员！");
+        }
 
         return CONGJSONResult.ok(orderId);
     }
